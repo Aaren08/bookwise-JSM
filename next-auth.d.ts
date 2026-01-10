a56cc2b47fc256b8e1e932c20e485fa08c344f9b
Aaren08 <schneiderneil392@gmail.com>
2026-01-10 16:06:53 +0500

    Make NextAuth session and JWT user properties optional and name nullable.

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,24 +2,24 @@ import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: string;
-      role: string;
-      name: string;
+    user?: {
+      id?: string;
+      role?: string;
+      name?: string | null;
     } & DefaultSession["user"];
   }
 
   interface User {
-    id: string;
-    role: string;
-    name: string;
+    id?: string;
+    role?: string;
+    name?: string | null;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string;
-    role: string;
-    name: string;
+    id?: string;
+    role?: string;
+    name?: string | null;
   }
 }
